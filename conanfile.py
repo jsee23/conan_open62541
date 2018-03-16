b0f21699e0881b776d241fc3110e740900d967bf
Jochen Seemann <seemann.jochen@gmail.com>
2018-03-16 21:19:00 +0100

    add initial conan packaging information

---
+++ conanfile.py
@@ -0,0 +1,38 @@
+from conans import ConanFile, CMake, tools
+
+
+class Open62541Conan(ConanFile):
+    name = "open62541"
+    version = "0.2"
+    license = "MPL v2"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Open62541 here>"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/open62541/open62541.git")
+        self.run("cd open62541 && git checkout 0.2")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="open62541")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s' % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="open62541/include")
+        self.copy("open62541.h", dst="include", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["open62541"]
